a16daa98ae8b19774deec0a2f095e87c44b7cb34
Navdeeppal Singh <s8nlsing@stud.uni-saarland.de>
2023-03-31 14:36:36 +0200

    chore: remove paths from settings

--- bcos/settings.py
+++ bcos/settings.py
@@ -4,7 +4,7 @@ General settings. Mainly paths to data.
 import os
 
 # data root (mainly for CIFAR10)
-DATA_ROOT = os.getenv("DATA_ROOT", "/BS/nsingh/nobackup/hiwi/data/")
+DATA_ROOT = os.getenv("DATA_ROOT")
 
 # ImageNet path
 IMAGENET_PATH = os.getenv("IMAGENET_PATH")
@@ -18,11 +18,7 @@ SHMTMPDIR = "/dev/shm"
 # Note this is not the same as the official ImageNet tar file
 # this expects each class to inside its own tar file, which will then be extracted
 # to SHMTMPDIR
-IMAGENET_TRAIN_TAR_FILES_DIR = os.getenv(
-    "IMAGENET_TRAIN_TAR_FILES_DIR", "/ptmp/nasingh/data/ImageNet_class_tars"
-)
+IMAGENET_TRAIN_TAR_FILES_DIR = os.getenv("IMAGENET_TRAIN_TAR_FILES_DIR")
 
 # Path to redis server
-REDIS_SERVER = os.getenv(
-    "REDIS_SERVER_PATH", "/raven/u/mboehle/mambaforge/envs/pl/bin/redis-server"
-)
+REDIS_SERVER = os.getenv("REDIS_SERVER_PATH")
