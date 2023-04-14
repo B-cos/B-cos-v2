730036d6c4c45c5e92f470baeb953cfdd810f906
Navdeeppal Singh <s8nlsing@stud.uni-saarland.de>
2023-04-14 15:56:11 +0200

    chore: bump version

--- bcos/version.py
+++ bcos/version.py
@@ -1,4 +1,4 @@
 # just for awesome version comparison, it's a dependency anyway
 from torch.torch_version import TorchVersion
 
-__version__ = TorchVersion("0.0.2")
+__version__ = TorchVersion("0.0.3")
