d7f9af2a51ab343469ab0392d24b3cdc9c88c091
Navdeeppal Singh <s8nlsing@stud.uni-saarland.de>
2023-04-17 13:13:02 +0200

    chore: bump version to `0.0.4`

--- bcos/version.py
+++ bcos/version.py
@@ -1,4 +1,4 @@
 # just for awesome version comparison, it's a dependency anyway
 from torch.torch_version import TorchVersion
 
-__version__ = TorchVersion("0.0.3")
+__version__ = TorchVersion("0.0.4")
