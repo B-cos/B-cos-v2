f1d6b4d0de28046948c8365c4c3b86e9d6a19c80
Tom Nuno Wolf <tn.wolf@tum.de>
2025-09-12 16:45:38 +0200

    triggering CI release

--- bcos/version.py
+++ bcos/version.py
@@ -1,4 +1,4 @@
 # just for awesome version comparison, it's a dependency anyway
 from torch.torch_version import TorchVersion
 
-__version__ = TorchVersion("0.1.2")
+__version__ = TorchVersion("0.1.3")
